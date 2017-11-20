1a89571f93bb9dbf8d4358ded501ec2fe459b89e
Joalbert González <joalbertgonzalez@gmail.com>
2017-11-19 20:14:00 -0500

    add title and anchor

--- js/app.js
+++ js/app.js
@@ -5,11 +5,36 @@ var form = document.getElementById('form');
 var nameList = document.getElementById('name-list');
 var button = document.getElementById('button-id');
 
-function createList(event) {
+function newList(event) {
   event.target.classList.toggle('display-none');
   form.classList.toggle('display-none');
 }
 
+function createList(event) {
+  event.preventDefault();
+
+  if(nameList.value) {
+    form.classList.toggle('display-none');
+
+    var title = nameList.value.toUpperCase();
+    var titleElement = document.createElement('div');
+
+    titleElement.textContent = title;
+
+    form.parentElement.insertBefore(titleElement, form);
+
+    var link = document.createElement("a");
+    link.setAttribute("href", "#");
+
+    var linkText = document.createTextNode("Añadir una tarjeta...");
+    link.appendChild(linkText);
+
+    /* link.addEventListener("click", createCard); */
+    titleElement.parentElement.appendChild(link);
+  }
+}
+
 function begin() {
-  addList.addEventListener('click', createList)
+  addList.addEventListener('click', newList);
+  button.addEventListener('click', createList);
 }
